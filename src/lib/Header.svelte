a0eeece13ca343a6438a9d86b6a6ff650e4a59a7
William Faircloth <willuhmjs@gmail.com>
2023-03-07 13:27:17 +0000

    feat: add hover animation

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,7 +3,7 @@
 	export let hintnumber = 1;
 </script>
 
-<h2 class="title">hintable</h2>
+<h2 class="title"><span id="glow">hint</span>able</h2>
 <div class="databox">
 	<h3>hints: {hintnumber}</h3>
 	<h3>guesses: {guesses}</h3>
@@ -14,6 +14,14 @@
 		font-size: 2rem;
 		text-align: center;
 		font-style: italic;
+	}
+
+	#glow:hover {
+		color: gold;
+	}
+
+	#glow {
+		transition: color 0.25s ease-in-out;
 	}
 
 	.databox {
