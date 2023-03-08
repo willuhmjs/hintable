20d64f6a277bda1eefc2a1a1656464fe36e43f17
William Faircloth <willuhmjs@gmail.com>
2023-03-08 01:50:15 +0000

    feat: add ellipse menu button

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,17 +3,38 @@
 	export let hintnumber = 4;
 </script>
 
-<h2 class="title"><span id="glow">hint</span>able</h2>
+<div class="titlebox">
+	<h2 class="title"><span id="glow">hint</span>able</h2>
+	<span class="menubutton"><i class="fa-solid fa-ellipsis-vertical" /></span>
+</div>
 <div class="databox">
 	<h3>hints remaining: {hintnumber}</h3>
 	<h3>guesses: {guesses}</h3>
 </div>
 
 <style>
+	.titlebox {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
 	.title {
 		font-size: 2rem;
 		text-align: center;
 		font-style: italic;
+		flex-grow: 1;
+		margin-left: 37.5px;
+	}
+
+	.menubutton {
+		text-align: right;
+		display: inline;
+		position: relative;
+		right: 0;
+		top: 0;
+		padding: 16px;
+		font-size: 22px;
 	}
 
 	#glow:hover {
