d55bd63884df382c383fce273800747bddc6f8e9
William Faircloth <willuhmjs@gmail.com>
2023-03-08 01:14:02 +0000

    chore: remove useless logging

--- src/routes/random/+page.ts
+++ src/routes/random/+page.ts
@@ -3,6 +3,5 @@ import words from '../../data/words';
 /** @type {import('./$types').PageLoad} */
 // return random from array
 export function load() {
-	console.log(words[Math.floor(Math.random() * words.length)]);
 	return words[Math.floor(Math.random() * words.length)];
 }
