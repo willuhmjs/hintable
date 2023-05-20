04095d106dbcac7fef40c38f3a9f6922b440d695
William Faircloth <willuhmjs@gmail.com>
2023-05-20 11:59:31 -0400

    refactor

---
+++ src/+layout.svelte
@@ -0,0 +1,42 @@
+<svelte:head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
+
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: #fffaf2;
+            touch-action: manipulation;
+            font-family: Montserrat, sans-serif;
+            color: #002147;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Primary Meta Tags -->
+    <title>hintable - an exciting word guessing game</title>
+    <meta name="title" content="hintable - an exciting word guessing game" />
+    <meta
+        name="description"
+        content="Can you guess a word with just five hints? Try this exciting game and put your vocabulary to the test!"
+    />
+    <meta name="theme-color" content="#fffaf2" />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://willuhmjs.github.io/hintable/" />
+    <meta property="og:title" content="hintable - an exciting word guessing game" />
+    <meta
+        property="og:description"
+        content="Can you guess a word with just five hints? Try this exciting game and put your vocabulary to the test!"
+    />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://willuhmjs.github.io/hintable/" />
+    <meta property="twitter:title" content="hintable - an exciting word guessing game" />
+    <meta
+        property="twitter:description"
+        content="Can you guess a word with just five hints? Try this exciting game and put your vocabulary to the test!"
+    />
+</svelte:head>
