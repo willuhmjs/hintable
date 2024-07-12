a3cc18c0755f339ff93b470d224d9ab5d7cc302e
William Faircloth <willuhmjs@gmail.com>
2024-07-12 18:22:52 -0400

    chore: fix tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 
 import { expect, test } from '@playwright/test';
-import words from "$lib/data/words.json"
+import words from "$lib/data/words.json" assert { type: "json" };
 
 test('index page has expected h2', async ({ page }) => {
 	await page.goto('/');
