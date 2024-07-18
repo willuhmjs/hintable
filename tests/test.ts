db433cf0d2b4a15018425c8dd90a008b11115281
William Faircloth <willuhmjs@gmail.com>
2024-07-18 02:01:56 +0000

    tests: fix

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 import words from '$lib/data/words.json' assert { type: 'json' };
 
+
 test('index page has expected h2', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'title' })).toBeVisible();
@@ -16,11 +17,11 @@ test('hint button works correctly', async ({ page }) => {
 });
 
 test('guessing latest word works correctly', async ({ page }) => {
-	const word: string = words[words.length - 1].word;
+	const word: string = words[0].word;
 	await page.goto('/');
 	await page.locator('#guessInput').fill(word);
 	await page.locator('#guessInput').press('Enter');
 	await expect(page.isVisible('#hint-5')).resolves.toBeTruthy();
 	await expect(page.locator('#hint-5').innerText()).resolves.toContain(word);
-	//await page.screenshot({ path: `screenshots/${word}.png` });
+	await page.screenshot({ path: `screenshots/${word}.png` });
 });
