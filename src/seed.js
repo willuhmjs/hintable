817b1dabccbc15bec23a78eb22faca85eddbdf1e
William Faircloth <willuhmjs@gmail.com>
2024-11-24 18:33:40 -0500

    seed cmd

---
+++ src/seed.js
@@ -0,0 +1,22 @@
+import { Difficulty, PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
+const word = {
+    "word": "hint",
+    "hintDb": [
+      "I can be subtle or obvious.",
+      "I am a helpful tool for learning or discovery.",
+      "'He couldn't catch the ____.'",
+      "I am a discreet cue that can help point someone in the right direction.",
+      "A word game is named after me."
+    ],
+    "difficulty": Difficulty.easy,
+    "day": new Date(1717017041259)
+  };
+
+  await prisma.game.upsert({
+    where: { word: word.word },
+    create: word,
+    update: {}
+  })
+
+  
