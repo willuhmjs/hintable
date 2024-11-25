e9ad3262ca98cee30f4614ce6c8d88b2630c8ddc
William Faircloth <willuhmjs@gmail.com>
2024-11-25 16:24:50 -0500

    fix random game tab

--- src/routes/random/+page.server.ts
+++ src/routes/random/+page.server.ts
@@ -4,29 +4,39 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 export const load = async () => {
-   const game = await prisma.game.findMany({
-     select: {
-          word: true,
-          difficulty: true,
-          hintDb: true,
-          id: true
-     },
-     take: 1,
-     skip: Math.floor(Math.random() * await prisma.game.count()),
-     orderBy: {
-         day: 'desc',
-     },
-     where: {
-          day: {
-               gte: new Date(0),
-               lte: dayjs(Date.now()).utc().startOf('day').toDate()
-          }
-     },
-});
+    const count = await prisma.game.count({
+        where: {
+            day: {
+                gte: new Date(0),
+                lte: dayjs(Date.now()).utc().startOf('day').toDate()
+            }
+        }
+    });
+
+    console.log(count);
+    const randomIndex = Math.floor(Math.random() * count) + 1;
+
+    const game = await prisma.game.findFirst({
+        select: {
+            word: true,
+            difficulty: true,
+            hintDb: true,
+            id: true
+        },
+        where: {
+            id: randomIndex,
+            day: {
+                gte: new Date(0),
+                lte: dayjs(Date.now()).utc().startOf('day').toDate()
+            }
+        }
+    });
+
     if (!game) {
-         return error(404, "Game not found");
+        return error(404, "Game not found");
     }
+
     return {
-          game: game[0]
+        game
     };
 };
