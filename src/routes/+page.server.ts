f72f46e6b5fe73edd07e56c53c4aef71e48a5cdd
William Faircloth <willuhmjs@gmail.com>
2024-11-24 20:29:44 -0500

    fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,22 +1,48 @@
 import { error } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma/prismaConnection.js';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
 export const load = async () => {
-   const game = await prisma.game.findFirst({
-     select: {
-          word: true,
-          difficulty: true,
-          hintDb: true,
-          id: true
-     },
-        orderBy: {
-            day: 'desc'
-        },
-        where: {
-            day: {
-                lte: new Date()
-            }
-        }
-});
+    const oldest = 0;
+	const newest = dayjs(Date.now()).utc().startOf('day').toDate().getTime();
+	const d = newest - oldest;
+	if (isNaN(d) || d < 0) {
+		error(400, 'Dates must be numbers, and oldest must be less than newest');
+	}
+    
+	if (newest > Date.now()) {
+		error(400, 'Newest date cannot be in the future');
+	}
+
+	const oldestDate = new Date(oldest);
+	const newestDate = new Date(newest);
+
+	const game = await prisma.game.findFirst({
+		where: {
+			day: {
+				gte: oldestDate,
+				lte: newestDate
+			}
+		},
+		select: {
+			word: true,
+			day: true,
+			difficulty: true,
+			hintDb: true
+		},
+		orderBy: {
+			day: 'desc'
+		}
+	});
+
+    if (!game) {
+         return error(404, "Game not found");
+    }
+    return {
+          game,
+          timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    };
     if (!game) {
          return error(404, "Game not found");
     }
