3ada16177e49c4b25f4ca87602b527148abaad06
William Faircloth <willuhmjs@gmail.com>
2024-11-28 15:59:37 -0500

    idk lol

--- src/routes/admin/backup/+server.ts
+++ src/routes/admin/backup/+server.ts
@@ -1,28 +1,5 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma/prismaConnection';
-import { redirect } from '@sveltejs/kit';
-import { JWT_SECRET } from '$env/static/private';
-
-import jwt from "jsonwebtoken";
-export const load = async ({ cookies }) => {
-	const user = cookies.get("session");
-
-	if (!user) {
-		throw redirect(303, '/login');
-	}
-
-    try {
-        const t = jwt.verify(user, JWT_SECRET);
-        if (t.username !== "admin") {
-            throw redirect(303, '/login');
-        }
-    } catch {
-        throw redirect(303, '/login');
-    }
-
-    return;
-
-};
 
 export const GET: RequestHandler = async ({ request }) => {
     try {
